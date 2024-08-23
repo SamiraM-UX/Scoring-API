db3fec331673ac3899db5bc935651caa556a2a76
SamiraM-UX <samiramahjoub25@gmail.com>
2024-08-23 10:14:41 +0200

    Update download_segments.py

--- download_segments.py
+++ download_segments.py
@@ -1,8 +1,19 @@
 import os
 import urllib.request
 
-# URL de base du fichier dans la release GitHub
-base_url = "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/"
+# Liste des URLs correctes pour chaque segment
+urls = [
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_0.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_1.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_2.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_3.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_4.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_5.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_6.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_7.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_8.joblib",
+    "https://github.com/SamiraM-UX/Scoring-API/releases/download/v1.1/df_train_smote_part_9.joblib"
+]
 
 # Répertoire cible pour enregistrer les segments
 save_dir = "/home/scoring/Scoring-API/saved_segments/"
@@ -12,15 +23,14 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
 # Télécharger chaque segment
-for i in range(10):
+for i, url in enumerate(urls):
     file_name = f"df_train_smote_part_{i}.joblib"
-    file_url = base_url + file_name
     file_path = os.path.join(save_dir, file_name)
 
     if not os.path.exists(file_path):
-        print(f"Téléchargement du {file_name} depuis {file_url}...")
+        print(f"Téléchargement du {file_name} depuis {url}...")
         try:
-            urllib.request.urlretrieve(file_url, file_path)
+            urllib.request.urlretrieve(url, file_path)
             print(f"{file_name} téléchargé et enregistré avec succès.")
         except urllib.error.HTTPError as e:
             print(f"Échec du téléchargement de {file_name}. HTTP Error: {e.code} - {e.reason}")
