9522d6b13e7c6a06dca320e1977699a4ef79e1b5
SamiraM-UX <samiramahjoub25@gmail.com>
2024-09-01 06:17:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ def predict():
     if sk_id_curr is None:
         print("Erreur : SK_ID_CURR est manquant dans la requête")
         return jsonify({"error": "SK_ID_CURR est manquant dans la requête"}), 400
+    
+    if not sk_id_curr.isdigit():
+        print(f"Erreur : SK_ID_CURR contient des caractères non numériques : {sk_id_curr}")
+        return jsonify({"error": "SK_ID_CURR doit être un entier numérique"}), 400
 
     if 'SK_ID_CURR' not in df_train_smote.columns:
         print("Erreur : La colonne 'SK_ID_CURR' n'existe pas dans le DataFrame")
@@ -53,53 +57,52 @@ def predict():
         print(f"Erreur : Aucun échantillon trouvé pour SK_ID_CURR: {sk_id_curr}")
         return jsonify({"error": f"Aucun échantillon trouvé pour SK_ID_CURR: {sk_id_curr}"}), 404
 
+
     # Mapping des noms de colonnes
     columns_mapping = {i: f"Column_{i}" for i in range(len(sample.columns))}
 
     # Renommer les colonnes du DataFrame
-    sample = sample.rename(columns=columns_mapping)
+    sample.rename(columns=columns_mapping, inplace=True)
 
     print(f"Colonnes après renommage : {sample.columns.tolist()}")
 
     # Garder uniquement les colonnes attendues par le modèle
     sample_for_prediction = sample[model_columns]
 
-    try:
-        print("Lancement de la prédiction")
-        prediction = model.predict_proba(sample_for_prediction)
-        proba = prediction[0][1]
-        print(f"Probabilité calculée : {proba}")
-        print("Prédiction terminée")
+    # Prédire
+    print("Lancement de la prédiction")
+    prediction = model.predict_proba(sample_for_prediction)
+    proba = prediction[0][1]
+    print(f"Probabilité calculée : {proba}")
+    print("Prédiction terminée")
+    # Calculer les valeurs SHAP pour l'échantillon donné
+    print("Calcul des valeurs SHAP")
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(sample_for_prediction)
 
-        # Calculer les valeurs SHAP pour l'échantillon donné
-        print("Calcul des valeurs SHAP")
-        explainer = shap.TreeExplainer(model)
-        shap_values = explainer.shap_values(sample_for_prediction)
+    if isinstance(shap_values, list) and len(shap_values) == 1:
+        shap_values = shap_values[0]
 
-        if isinstance(shap_values, list) and len(shap_values) == 1:
-            shap_values = shap_values[0]
 
-        # Limiter les données renvoyées pour la lisibilité (par exemple, les 10 premières features)
-        num_features_to_show = 10
-        limited_shap_values = shap_values[0][:num_features_to_show].tolist()
-        limited_feature_names = sample_for_prediction.columns[:num_features_to_show].tolist()
-        limited_feature_values = sample_for_prediction.values[0][:num_features_to_show].tolist()
+    # Limiter les données renvoyées pour la lisibilité (par exemple, les 10 premières features)
+    num_features_to_show = 10
+    limited_shap_values = shap_values[0][:num_features_to_show].tolist()
+    limited_feature_names = sample_for_prediction.columns[:num_features_to_show].tolist()
+    limited_feature_values = sample_for_prediction.values[0][:num_features_to_show].tolist()
 
-        print("Réponse générée avec succès")
+    print("Réponse générée avec succès")
 
-        return jsonify({
-            'probability': round(proba * 100, 2),
-            'shap_values': limited_shap_values,
-            'feature_names': limited_feature_names,
-            'feature_values': limited_feature_values
-        })
-    except Exception as e:
-        print(f"Erreur lors de la prédiction : {e}")
-        return jsonify({"error": "Une erreur est survenue lors de la prédiction."}), 500
+    return jsonify({
+        'probability': round(proba * 100, 2),
+        'shap_values': limited_shap_values,
+        'feature_names': limited_feature_names,
+        'feature_values': limited_feature_values
+    })
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
     print(f"Lancement de l'application sur le port {port}")
     app.run(debug=False, host="0.0.0.0", port=int(port))
+
 
 
